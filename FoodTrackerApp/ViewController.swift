a0c82a0161bc331641447fd2103f9178872ce534
Yemisi <yonayemi@yahoo.com>
2019-02-28 15:16:37 -0500

    Initial Commit

---
+++ FoodTrackerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTrackerApp
+//
+//  Created by Yemisi  on 2/28/19.
+//  Copyright © 2019 Yemisi . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
