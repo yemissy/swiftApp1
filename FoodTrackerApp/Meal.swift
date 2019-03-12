8144698164de5ba7dd5fb519adf54363b25a8093
Yemisi <yonayemi@yahoo.com>
2019-03-12 16:51:59 -0400

    Completed unit testing for each instance of the meal class

--- FoodTrackerApp/Meal.swift
+++ FoodTrackerApp/Meal.swift
@@ -13,19 +13,31 @@ class Meal{
     //MARK:Properties
     
     var name: String
-    var image: UIImage?
+    var photo: UIImage?
     var rating: Int
     
     
     //MARK: Initializer
     
-    init?(name:String, image:UIImage, rating:Int){
+    init?(name:String, photo:UIImage?, rating:Int){
+        
         // Initialization should fail if there is no name or if the rating is negative.
-        if name.isEmpty || rating < 0 {
+        
+        //A guard statement declares a condition that must be true in order for the code after the guard statement to be executed. If the condition is false, the guard statement’s else branch must exit the current code block (for example, by calling return, break, continue, throw, or a method that doesn’t return like fatalError(_:file:line:)).
+        
+        //Name must not be empty
+        guard !name.isEmpty else {
             return nil
         }
+        
+       // The rating must be between 0 and 5 inclusively
+        guard (rating >= 0) && (rating <= 5 ) else{
+            return nil 
+        }
+        
+        //Initialize stored properties
         self.name = name
-        self.image = image
+        self.photo = photo
         self.rating = rating
     }
 }
--- FoodTrackerAppTests/FoodTrackerAppTests.swift
+++ FoodTrackerAppTests/FoodTrackerAppTests.swift
@@ -11,24 +11,35 @@ import XCTest
 
 class FoodTrackerAppTests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    //MARK: Meal Class Test
+    
+    //Confirm that the meal initializer returns a meal object when passed valid parameters
+    func testMealInitializationSuceeds(){
+        
+        //Zero rating
+        let zeroRatingMeal = Meal.init(name: "friedRice", photo: nil, rating: 0)
+        XCTAssertNotNil(zeroRatingMeal)
+        
+        //Highest Positive Rating
+        let positiveRatingMeal = Meal.init(name: "friedRice", photo: nil, rating: 5)
+        XCTAssertNotNil(positiveRatingMeal)
     }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testMealInitializationFails(){
+        //Confirm that the meal initializer returns nil when a negative rating is passed or an empty name is passed
+        
+        //Negative Rating
+        let negativeRating = Meal.init(name: "friedRice", photo: nil, rating: -1)
+        XCTAssertNil(negativeRating)
+        
+        
+        //Rating Exceeds Maximum
+        let largeRating = Meal.init(name: "rice", photo: nil, rating: 6)
+        XCTAssertNil(largeRating)
+        
+        //Empty string
+        let emptyString = Meal.init(name: "", photo: nil, rating: 5)
+        XCTAssertNil(emptyString)
     }
 
 }
